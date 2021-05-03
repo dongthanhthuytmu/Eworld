b3c09a52a68c2ceeca26a25c16db1362b77eb859
Thanh-Th <thanhthuy29122000@gmail.com>
2021-05-03 11:44:01 +0700

    myProject

---
+++ backend/views/layouts/main.php
@@ -0,0 +1,4 @@
+
+<?php include_once "header.php"; ?>
+<?php echo $this->content; ?>
+<?php include_once "footer.php"; ?>
---
+++ fontend/models/User.php
@@ -0,0 +1,46 @@
+<?php 
+require_once 'models/Model.php';
+class User extends Model {
+
+	public function getUser($email, $password) {
+		$obj_user = $this->connection->prepare("SELECT * FROM users WHERE email=:email AND password=:password AND role_id != 1");
+		$arr_user = [
+			':email' => $email,
+			':password' => $password
+		];
+		$obj_user->execute($arr_user);
+		$user = $obj_user->fetch(PDO::FETCH_ASSOC);
+		return $user;
+
+	}
+
+	public function isExistUsername($email) {
+		$obj_select_one = $this->connection->prepare("SELECT * FROM users WHERE email = :email");
+		$select = [':email' => $email];
+		$obj_select_one->execute($select);
+		$user = $obj_select_one->fetch(PDO::FETCH_ASSOC);
+
+		if (!empty($user)) {
+			return TRUE;
+		}
+		return FALSE;
+	}
+
+
+	public function register($first_name, $last_name, $phone, $email, $password, $address) {
+		$obj_insert = $this->connection->prepare('INSERT INTO users(first_name, last_name, phone, email, password, address) VALUES (:first_name, :last_name, :phone, :email, :password, :address)');
+		$inserts = [
+			':first_name' => $first_name,
+			':last_name' => $last_name,
+			':phone' => $phone,
+			':email' => $email,
+			':password' => $password,
+			':address' => $address
+		];
+		return $obj_insert->execute($inserts);
+	}
+ 
+}
+
+
+?>
